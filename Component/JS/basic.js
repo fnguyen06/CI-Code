d171456ef97b06c612fc4980ce332205dc3aecd2
pluderes <kingofpoppro@gmail.com>
2021-07-17 00:47:08 +0700

    fixed width component

--- Component/JS/basic.js
+++ Component/JS/basic.js
@@ -17,35 +17,11 @@ class basic extends HTMLElement {
 
   connectedCallback() {
     //mounting
-    const inner = `    <link
-    rel="stylesheet"
-    href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
-    crossorigin="anonymous"
-  />
-  <link
-    rel="stylesheet"
-    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
-  />
-  <script
-    src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-    crossorigin="anonymous"
-  ></script>
-  <script
-    src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-    integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-    crossorigin="anonymous"
-  ></script>
-  <script
-    src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-    integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-    crossorigin="anonymous"
-  ></script>
+    const inner = `
         ${styleInLine}
-        <div class="componen col-sm-3">
+        <div class="componen col-12">
           <div class="img">
-            <img src="${this.props.avatar}" alt="" style="max-width: 100%" />
+            <img src="${this.props.avatar}" alt=""/>
           </div>
           <div class="block-description">
             <h6 class="title">
